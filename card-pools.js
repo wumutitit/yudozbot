1b848d98076c6a8c522e85b6149666fb06799b7b
대한Daehan <129164199+wumutitit@users.noreply.github.com>
2026-04-04 07:48:20 +0800

    card-pools.js

--- card-pools.js
+++ card-pools.js
@@ -1,9 +1,9 @@
 {
 "all_themes": [
     {
-     "card1": "ILYN_INCA;ILMJ_INCA_ILWH_INCA;ILMK_INCA;ILIR_INCA",
-     "card2": "ILIR_INCA;ILMK_INCA;ILMJ_INCA;ILYN_INCA;ILWH_INCA",
-     "card3": "ILMK_INCA;ILIR_INCA;ILYN_INCA;ILWH_INCA;ILMJ_INCA"
+     "card1": "ILYN_INCA;ILMJ_INCA;KIJL_REM;KINT_REM;NWSO_STDY;NWRK_STDY;NWYS_STDY",
+     "card2": "ILWH_INCA;ILMK_INCA;KIBE_REM;NWJH_STDY;NWRY_STDY",
+     "card3": "ILIR_INCA;KIHN_REM;NWSK_STDY"
       }
       ]
                              }
